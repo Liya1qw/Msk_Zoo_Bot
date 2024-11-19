90e3d33d0e0d706e3c2aa068e89ac381a6891476
Liya1qw <natarasova11186@gmail.com>
2024-11-19 18:50:27 +0300

    Delete zoo_bot directory

--- zoo_bot/utils.py
+++
@@ -1,9 +0,0 @@
-from aiogram import Bot, Dispatcher
-from aiogram.contrib.fsm_storage.memory import MemoryStorage
-
-TOKEN = '7671646018:AAGJZX9cZDZZ8kC3MpOdT9pSLMoaE1LW7i0'
-
-storage = MemoryStorage()
-bot = Bot(token=TOKEN)
-dp = Dispatcher(bot, storage=storage)
-
